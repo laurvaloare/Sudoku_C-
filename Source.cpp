6157c80417bff42dd8c6302885da20ce40d02acd
laurvaloare <48419946+laurvaloare@users.noreply.github.com>
2019-03-10 22:57:18 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int sudoku[9][9] = { { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+	{ 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+	{ 0, 9, 8, 0, 0, 0, 0, 0, 6 },
+	{ 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+	{ 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+	{ 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+	{ 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+	{ 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+	{ 0, 0, 0, 0, 8, 0, 0, 7, 9 } };
+
+
+
+bool VerificareRand(int sudoku[9][9], int rand, int valoare);
+bool VerificareCol(int sudoku[9][9], int col, int valoare);
+bool VerificarePatrat(int sudoku[9][9], int rand, int col, int valoare);
+bool Rezolvat(int sudoku[9][9], int &rand, int &col);
+bool Rezolvare(int sudoku[9][9]);
+
+int main() {
+
+	if (Rezolvare(sudoku)==true)
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+				cout << sudoku[i][j];
+
+			cout << endl;
+		}
+	}
+	else
+		cout << "nu e sudoku";
+
+	_getch();
+
+	return 0;
+}
+
+bool VerificareRand(int sudoku[9][9], int rand,int valoare) {
+
+	for ( int col = 0;  col < 9;  col++)
+		if (sudoku[rand][col] == valoare)
+			return false;
+
+		
+	
+	return true;
+		
+	
+
+}
+
+bool VerificareCol(int sudoku[9][9], int col, int valoare) {
+
+	for (int rand = 0; rand < 9; rand++)
+		if (sudoku[rand][col] == valoare)
+			return false;
+
+		
+	return true;
+
+	
+
+}
+
+bool VerificarePatrat(int sudoku[9][9], int rand, int col, int valoare) {
+
+	for (int i = 0; i < 9; i++)
+		for (int j = 0; j < 9; j++)
+			if (sudoku[i + rand][j + col] == valoare)
+				return false;
+
+	return true;
+
+}
+
+bool Rezolvat(int sudoku[9][9], int &rand, int &col) {
+
+	for (rand = 0; rand< 9; rand++)
+		for (col = 0; col < 9; col++)
+			if (sudoku[rand][col] == 0)
+				return false;
+			
+	return true;
+
+}
+
+bool Rezolvare(int sudoku[9][9])
+{
+
+	int rand = 0;
+	int col = 0;
+
+	if (Rezolvat(sudoku,rand,col))
+		return true;
+
+	for (int valoare = 1;valoare <= 9;valoare++) {
+
+		if ((VerificareCol(sudoku, col, valoare)) && (VerificareRand(sudoku, rand, valoare)) && VerificarePatrat(sudoku, rand - rand % 3, col - col % 3, valoare)&&(sudoku[rand][col]==0)==true) 
+		{
+
+			sudoku[rand][col] = valoare;
+
+			if (Rezolvare(sudoku))
+				return true;
+
+			sudoku[rand][col] = 0;
+		}
+	}
+	return false;
+}
+
